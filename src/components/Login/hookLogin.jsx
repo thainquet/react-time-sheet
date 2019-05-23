34667cabcac8d6ac61776cb0d64938f79a7b9957
thainq-teko <thai.nq@teko.vn>
2019-05-23 17:33:06 +0700

    fix validation on password input

--- src/components/Login/hookLogin.jsx
+++ src/components/Login/hookLogin.jsx
@@ -3,24 +3,43 @@ import React, { useState, useGlobal } from 'reactn';
 
 
 const hookLogin = props => {
+    // handle username
     const [username, setUsername] = useState('')
-    const [messageUsername, setMessage] = useState('')
+    const [messageUsername, setUsernameMessage] = useState('')
 
-    const handleInput = (event) => {
+    const handleUsernameInput = (event) => {
         let input = event.target.value;
         let regexTV = /^[ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ]+$/
         let regexSpace = /\s+/g
         if (!input) {
-            setMessage(messageUsername => messageUsername = "ko dc de trong")
+            setUsernameMessage(messageUsername => messageUsername = "ko dc de trong")
         }
         else {
-            if (input.length < 6 || regexTV.test(input)) {                
-                setMessage(messageUsername => messageUsername = "gom 6 ky tu viet thuong tro len, viet lien ko dau")
+            if (input.length < 6 || regexTV.test(input) || regexSpace.test(input)) {                
+                setUsernameMessage(messageUsername => messageUsername = "gom 6 ky tu viet thuong tro len, viet lien ko dau")
             }
             else {
-                console.log(input)
                 setUsername(username => username = input)
-                setMessage(messageUsername => messageUsername = '')
+                setUsernameMessage(messageUsername => messageUsername = '')
+            }
+        }
+    }
+    //handle password
+    const [password, setPassword] = useState('')
+    const [messagePassword, setPasswordMessage] = useState('')
+
+    const handlePasswordInput = event => {
+        let input = event.target.value;
+        let regexCapital = /^[a-zA-Z0-9]+$/
+        if (!input) setPasswordMessage(messagePassword => messagePassword = "ko dc de trong")
+        else {
+            if (input.length < 8 || /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])/.test(input) != true) {          
+                setPasswordMessage(messagePassword => messagePassword = "gom it nhat 1 chu thuong 1 chu hoa 1 chu so")
+                return
+            }
+            else {
+                setPassword(password => password = input)
+                setPasswordMessage(messagePassword => messagePassword = '')
             }
         }
     }
@@ -29,22 +48,24 @@ const hookLogin = props => {
         <div>
             <div>
                 <label htmlFor="">Username</label>
-                <input type="text" autoFocus autoComplete="off" name="username" onBlur={(event) => handleInput(event)} />
+                <input type="text" autoFocus autoComplete="off" name="username" onChange={(event) => handleUsernameInput(event)} />
                 <br />
                 <span>
                     {messageUsername && <b>{messageUsername}</b>}
                 </span>
             </div>
-            {/* <div>
+
+            <div>
                 <label htmlFor="">Password</label>
-                <input type="text" autoComplete="off" name="username" onBlur={(event) => handleInput(event)} />
+                <input type="password" autoComplete="off" name="username" onChange={(event) => handlePasswordInput(event)} />
                 <br />
                 <span>
-                    {message && <b>{message}</b>}
+                    {messagePassword && <b>{messagePassword }</b>}
                 </span>
-            </div> */}
+            </div>
+
             <button onClick={() => {
-                messageUsername ? console.log('sai') : console.log({username})
+                (messageUsername || messagePassword) ? console.log('input k hop le') : console.log({username}, {password})
             }}>send</button>
         </div>
     )
