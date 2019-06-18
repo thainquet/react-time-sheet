e8c68361424c8a90ad1799eaa335746b11c395ce
thainq-teko <thai.nq@teko.vn>
2019-06-18 17:08:29 +0700

    temp

--- src/components/Login/MainPage.jsx
+++ src/components/Login/MainPage.jsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 
 import Form from 'react-bootstrap/Form'
 import { Link } from 'react-router-dom'
-import { withFormik } from 'formik'
 
 const MainPage = () => {
   return (
